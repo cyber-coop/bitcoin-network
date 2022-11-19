85d4617f6172b1809ce270a430dceeb2dc60b756
ethicnology <ethicnology@pm.me>
2022-11-19 18:57:05 +0100

    test: Inventory serialize/deserialize

--- src/inventory.rs
+++ src/inventory.rs
@@ -18,3 +18,47 @@ impl Inventory {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_inventory_serialize() {
+        let mut hash =
+            hex::decode("5bf400bf44ac7a7cb0542ee7e3f9374f68be2dfdf0d64a654c2def6288b3936b")
+                .unwrap();
+        hash.reverse();
+        let bytes_reversed_hash = hash.try_into().unwrap();
+        assert_eq!(
+            Inventory {
+                identifier: 1,
+                hash: bytes_reversed_hash,
+            }
+            .serialize(),
+            [
+                1, 0, 0, 0, 107, 147, 179, 136, 98, 239, 45, 76, 101, 74, 214, 240, 253, 45, 190,
+                104, 79, 55, 249, 227, 231, 46, 84, 176, 124, 122, 172, 68, 191, 0, 244, 91,
+            ]
+        );
+    }
+
+    #[test]
+    fn test_inventory_deserialize() {
+        let mut hash =
+            hex::decode("5bf400bf44ac7a7cb0542ee7e3f9374f68be2dfdf0d64a654c2def6288b3936b")
+                .unwrap();
+        hash.reverse();
+        let bytes_reversed_hash = hash.try_into().unwrap();
+        assert_eq!(
+            Inventory::deserialize(&[
+                1, 0, 0, 0, 107, 147, 179, 136, 98, 239, 45, 76, 101, 74, 214, 240, 253, 45, 190,
+                104, 79, 55, 249, 227, 231, 46, 84, 176, 124, 122, 172, 68, 191, 0, 244, 91,
+            ]),
+            Inventory {
+                identifier: 1,
+                hash: bytes_reversed_hash,
+            }
+        );
+    }
+}
