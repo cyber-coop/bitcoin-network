288d6776ed4aeae9f03cea7baeb351ec80fcc560
rllola <rllola80@gmail.com>
2022-11-22 10:39:29 +0100

    fix Tx; added deserialize for TxOut and TxIn;

--- src/tx.rs
+++ src/tx.rs
@@ -17,6 +17,8 @@ impl Tx {
     pub fn deserialize_with_size(bytes: &[u8]) -> (Tx, usize) {
         let mut offset = 0;
 
+        dbg!(hex::encode(&bytes));
+
         let version = u32::from_le_bytes(bytes[offset..offset+4].try_into().unwrap());
         offset += 4;
 
@@ -25,14 +27,13 @@ impl Tx {
         let varint_size = VarInt::get_size(count).unwrap();
         offset += varint_size as usize; 
 
-        let mut tx_ins : Vec<TxIn> = vec![];
-        for n in 0..count-1 {
-            let size = VarInt::decode(&bytes[offset..offset+9]).unwrap() as usize;
-            let varint_size = VarInt::get_size(count).unwrap();
-            offset += varint_size as usize; 
+        dbg!(count);
 
-            let tx_in = TxIn::deserialize(&bytes[offset..offset+size]);
+        let mut tx_ins : Vec<TxIn> = vec![];
+        for _n in 1..count {
+            let (tx_in, size) = TxIn::deserialize_with_size(&bytes[offset..]);
             offset += size;
+            dbg!(size);
 
             tx_ins.push(tx_in);
         }
@@ -42,13 +43,11 @@ impl Tx {
         let varint_size = VarInt::get_size(count).unwrap();
         offset += varint_size as usize; 
 
-        let mut tx_outs : Vec<TxOut> = vec![];
-        for n in 0..count-1 {
-            let size = VarInt::decode(&bytes[offset..offset+9]).unwrap() as usize;
-            let varint_size = VarInt::get_size(count).unwrap();
-            offset += varint_size as usize; 
+        dbg!(count);
 
-            let tx_out = TxOut::deserialize(&bytes[offset..offset+size]);
+        let mut tx_outs : Vec<TxOut> = vec![];
+        for _n in 1..count {
+            let (tx_out, size) = TxOut::deserialize_with_size(&bytes[offset..]);
             offset += size;
 
             tx_outs.push(tx_out);
@@ -71,27 +70,98 @@ impl Tx {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct TxIn {}
+pub struct TxIn {
+    pub previous_output: Outpoint,
+    pub signature_script: Vec<u8>,
+    pub sequence: u32,
+}
 
 impl TxIn {
     pub fn serialize(&self) -> Vec<u8> {
         todo!();
     }
 
+    pub fn deserialize_with_size(bytes: &[u8]) -> (TxIn, usize) {
+        let mut offset = 0;
+
+        let previous_output = Outpoint::deserialize(&bytes[0..36]);
+        offset += 36;
+
+        let size = VarInt::decode(&bytes[offset..offset+9]).unwrap();
+        let varint_size = VarInt::get_size(size).unwrap();
+        offset += varint_size as usize;
+
+        let signature_script = bytes[offset..offset+(size as usize)].to_vec();
+        offset += size as usize;
+
+        let sequence = u32::from_le_bytes(bytes[offset..offset+4].try_into().unwrap());
+        offset += 4;
+
+        (Self {
+            previous_output,
+            signature_script,
+            sequence,
+        }, offset)
+    }
+
     pub fn deserialize(bytes: &[u8]) -> TxIn {
-        Self {}
+        Self::deserialize_with_size(bytes).0
     }
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct TxOut {}
+pub struct Outpoint {
+    pub previous_hash: [u8; 32],
+    pub index: u32,
+}
+
+impl Outpoint {
+    pub fn serialize(&self) -> Vec<u8> {
+        todo!();
+    }
+
+    pub fn deserialize(bytes: &[u8]) -> Outpoint {
+        let previous_hash = bytes[0..32].try_into().unwrap();
+        let index = u32::from_le_bytes(bytes[32..36].try_into().unwrap());
+
+        Self {
+            previous_hash,
+            index,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct TxOut {
+    pub value: i64,
+    pub pk_script: Vec<u8>,
+}
 
 impl TxOut {
     pub fn serialize(&self) -> Vec<u8> {
         todo!();
     }
 
+    pub fn deserialize_with_size(bytes: &[u8]) -> (TxOut, usize) {
+        let mut offset = 0;
+
+        let value = i64::from_le_bytes(bytes[0..8].try_into().unwrap());
+        offset += 8;
+
+        let size = VarInt::decode(&bytes[offset..offset+9]).unwrap();
+        let varint_size = VarInt::get_size(size).unwrap();
+        offset += varint_size as usize;
+
+        let pk_script = bytes[offset..offset+(size as usize)].to_vec();
+        offset += size as usize;
+
+        (Self {
+            value,
+            pk_script,
+        }, 0)
+    }
+
     pub fn deserialize(bytes: &[u8]) -> TxOut {
-        Self {}
+        Self::deserialize_with_size(bytes).0
     }
 }
