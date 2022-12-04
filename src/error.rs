f66f9e440cc2378ad9b8ffa380c574fb9f54b314
rllola <rllola80@gmail.com>
2022-12-04 23:27:24 +0100

    remove some uft8 strings

--- src/error.rs
+++ src/error.rs
@@ -12,16 +12,8 @@ impl Display for DeserializeError {
 
 impl Error for DeserializeError {}
 
-impl<const N: usize> From<std::array::IntoIter<u8, N>> for DeserializeError {
-    fn from(_e: std::array::IntoIter<u8, N>) -> Self { DeserializeError("Failed to read bytes".to_owned()) }
-}
-
 impl From<std::io::Error> for DeserializeError {
     fn from(_e: std::io::Error) -> Self { DeserializeError("Failed to read varint".to_owned()) }
-}
-
-impl From<usize> for DeserializeError {
-    fn from(_e: usize) -> Self { DeserializeError("Failed to advance bytes".to_owned()) }
 }
 
 impl From<std::string::FromUtf8Error> for DeserializeError {
